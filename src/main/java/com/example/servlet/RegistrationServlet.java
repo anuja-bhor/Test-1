c1414fd9a25ed8d32ce1bb5b7c21be5661e37528
root <root@ip-172-31-7-195.ap-south-1.compute.internal>
2025-01-20 09:29:13 +0000

    uploaded project1

---
+++ src/main/java/com/example/servlet/RegistrationServlet.java
@@ -0,0 +1,19 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        response.getWriter().println("Registration Successful! Name: " + name + ", Email: " + email);
+    }
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Form</title>
+</head>
+<body>
+    <h1>Register</h1>
+    <form action="register" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" required><br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required><br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
+
